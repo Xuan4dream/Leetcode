74df144d8be61e0b5ad592fa1595352953bd1c92
Xuan4dream <36721245+Xuan4dream@users.noreply.github.com>
2021-04-03 14:01:54 -0700

    Initial version of 169. Majority Element

---
+++ 169. Majority Element.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # 04032021 SECOND TRY with reference to Approach 2
+        counts = collections.Counter(nums)
+        return max(counts.keys(), key = counts.get)
+        # Time: O(N)
+        # Space: O(N)
+         
+#         # 04032021 FIRST TRY
+#         d = {}
+#         majority_count = len(nums)/2
+        
+#         for num in nums:
+#             if num in d:
+#                 d[num] = d[num] + 1
+#             else: 
+#                 d[num] = 1
+        
+#         for k in d.keys():
+#             if d[k] > majority_count:
+#                 return k
+#         # Time: O(N) 
+#         # Space: O(N)
+
